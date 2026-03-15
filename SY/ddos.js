b6e9b19aecee5e00ce57aff76e263baf0d018e87
SABIR 7718 <s04006241@gmail.com>
2026-03-15 05:49:53 +0000

    Add files via upload

---
+++ SY/ddos.js
@@ -0,0 +1,25 @@
+/*
+ * © 2026 SeXyxeon (VOIDSEC)
+ *
+ * ⚠️ COPYRIGHT NOTICE
+ * This source code is protected under copyright law.
+ * Any form of re-uploading, recoding, modification,
+ * selling, or redistribution WITHOUT explicit permission
+ * from the original author is strictly prohibited.
+ *
+ * ❌ NO CREDIT = NO PERMISSION
+ * ❌ DO NOT CLAIM THIS CODE AS YOUR OWN
+ *
+ * ✔️ Usage or modification is allowed ONLY
+ * with prior permission and proper credit.
+ *
+ * OFFICIAL LINKS (ONLY):
+ * YouTube   : https://youtube.com/@voidsec7718
+ * Instagram : sabir._7718
+ * Telegram  : https://t.me/SABIR7718
+ * GitHub    : https://github.com/SABIR7718
+ * WhatsApp  : +91 73650 85213
+ *
+ * Violations may result in DMCA takedown
+ * or termination of the Telegram bot.
+ */
